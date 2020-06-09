aed0a9a5c197b959365c54d1c59b231fa70fa38b
zhangcn <15236674712@163.com>
2020-06-09 10:02:20 +0800

    这是第一次提交

---
+++ src/main/java/com/baizhi/yingx190_git/Yingx190GitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.yingx190_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yingx190GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yingx190GitApplication.class, args);
+    }
+
+}
